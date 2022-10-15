fc50021963b541939998afba0182aec52253a5df
Sneh Patel <spatel805@gatech.edu>
2022-10-15 14:12:09 -0400

    created a constuctor with a difficulty and name variable with getters and setters for both variables

--- src/main/java/com/group64/settingspartone.java
+++ src/main/java/com/group64/settingspartone.java
@@ -1,4 +1,28 @@
 package com.group64;
 
 public class settingspartone {
+    private String ign;
+    private String difficulty;
+
+    public settingspartone() {
+        ign = "";
+        difficulty = "easy";
+    }
+
+    public boolean validName() {
+        if (ign == "" || ign == null || ign == " " ) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public String getIgn() {
+        return ign;
+    }
+
+    public void setIgn(String in) {
+        ign = in;
+    }
+
 }
