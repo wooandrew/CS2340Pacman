2f7780a9d1f30f3db760ab94eb9c712f522e8b5a
Sneh Patel <spatel805@gatech.edu>
2022-10-16 22:02:59 -0400

    created feature for player name.

--- src/main/java/com/group64/App.java
+++ src/main/java/com/group64/App.java
@@ -1,8 +1,15 @@
 package com.group64;
 
 import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -17,7 +24,7 @@ public class App extends Application {
         var javafxVersion = SystemInfo.javafxVersion();
 
         var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        var scene = new Scene(new StackPane(label), 800, 500);
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/group64/settingspartone.java
+++ src/main/java/com/group64/settingspartone.java
@@ -1,34 +1,32 @@
 package com.group64;
 
+import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class settingspartone {
+import static javafx.application.Application.launch;
+
+public class settingspartone extends Application {
     private String ign;
     private String difficulty;
 
     public settingspartone() {
-        ign = "";
+        ign = null;
         difficulty = "easy";
     }
 
-    //runs the actual game
-    public void settingscene(Stage stage) {
-        VBox setttingNode = new VBox();
-        Scene settingsScene =  new Scene(setttingNode);
-
-
-
-        stage.setScene(settingsScene);
-        stage.show();
-    }
-
-    public boolean validName() {
-        if (ign == "" || ign == null || ign == " " ) {
-            return false;
-        } else {
+    public boolean badName(String str) {
+        if (str.isEmpty() || str.trim().length() == 0 || str == null) {
             return true;
+        } else {
+            return false;
         }
     }
 
@@ -46,6 +44,46 @@ public class settingspartone {
 
     public void setDifficulty(String in2) {
         difficulty = in2;
+    }
+
+    @Override
+    public void start(Stage game) {
+        game.setTitle("pacman settings");
+        game.setWidth(600);
+        game.setHeight(800);
+
+        GridPane pane = new GridPane();
+        pane.setPadding(new Insets(10, 10, 10, 10));
+        pane.setVgap(5);
+        pane.setHgap(5);
+
+        TextField name = new TextField();
+        name.setPromptText("Enter name.");
+        pane.add(name, 70, 50, 1, 1);
+
+        Button submit = new Button("Submit");
+        pane.add(submit, 72, 50, 2, 1);
+
+        Label check = new Label();
+        pane.add(check, 70, 51, 1, 1);
+
+        submit.setOnAction(e -> {
+            if (badName(name.getText())) {
+                check.setText("not a valid name");
+            } else {
+                check.setText("Thanks");
+                setIgn(name.getText());
+            }
+        });
+
+        Scene settings = new Scene(pane);
+        game.setScene(settings);
+        game.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 
 }
