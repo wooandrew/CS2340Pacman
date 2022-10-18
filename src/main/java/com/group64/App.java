29876447757a1be7dee53f7fbbacc0a10fe3bf0e
Faiq Ahmad <faiqahmad@Faiqs-MacBook-Air-2.local>
2022-10-18 14:23:34 -0400

    checkpoint

--- src/main/java/com/group64/App.java
+++ src/main/java/com/group64/App.java
@@ -2,6 +2,7 @@ package com.group64;
 
 import javafx.application.Application;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -9,9 +10,13 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
 
 /**
  * JavaFX App
@@ -19,47 +24,12 @@ import java.io.FileNotFoundException;
 public class App extends Application {
 
     @Override
-    public void start(Stage stage) throws FileNotFoundException {
-//        var javaVersion = SystemInfo.javaVersion();
-//        var javafxVersion = SystemInfo.javafxVersion();
-//
-//        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-//        var scene = new Scene(new StackPane(label), 640, 480);
-//        stage.setScene(scene);
-//        stage.show();
+    public void start(Stage stage) throws IOException {
 
-        double scale = 0.5;
-
-        Image background = new Image(new
-                FileInputStream("/Users/faiqahmad/" +
-                "Desktop/CS 2340/pacman/src/main/java/com" +
-                "/group64/backgroundSettings.png"));
-
-        ImageView backgroundImageView = new ImageView(background);
-
-        backgroundImageView.setFitWidth(600);
-        backgroundImageView.setFitHeight(800);
-
-        Group root = new Group(backgroundImageView);
-
-        Scene settingsScene = new Scene(root, Color.BLACK);
-
-
-
-        stage.setMinHeight(800);
-        stage.setMaxHeight(800);
-        stage.setMinWidth(600);
-        stage.setMaxWidth(600);
-
-
-
-
-        String javaVersion = SystemInfo.javaVersion();
-        String javafxVersion = SystemInfo.javafxVersion();
-        Label label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        stage.setTitle("!PACMAN!");
-        stage.setWidth(600);
-        stage.setHeight(800);
+        URL url = new File("src/main/java/com/group64/settings.fxml").toURI().toURL();
+        Parent root = FXMLLoader.load(url);
+//        Parent root = FXMLLoader.load(getClass().getResource("/settings.fxml"));
+        Scene settingsScene = new Scene(root);
         stage.setScene(settingsScene);
         stage.show();
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,5 @@
 module com.group64 {
     requires javafx.controls;
+    requires javafx.fxml;
     exports com.group64;
 }
