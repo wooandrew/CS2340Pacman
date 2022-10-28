ecc98a787293cd93bb20ff0d8bccae16fcbbff32
Andrew Woo <awoo32@gatech.edu>
2022-10-28 16:30:21 -0400

    PacMan character class update

--- src/main/java/com/group64/Character.java
+++ src/main/java/com/group64/Character.java
@@ -1,18 +1,33 @@
 package com.group64;
 
+import javafx.geometry.Dimension2D;
+import javafx.scene.image.Image;
+
 public class Character {
     
-    private int lives;
+    private int lives;                      // Number of lives player has
+    private Dimension2D position;           // Position in x,y coords
+    private Dimension2D size;               // Size of character, in pixels
+    private Image[] sprites;                // Array of character sprites
 
-    public Character(int lives) {
+    public Character(int lives, Dimension2D pos, Dimension2D size) {
         this.lives = lives;
+        this.position = pos;
+        this.size = size;
     }
 
     public int getLives() {
         return lives;
     }
+
     public void setLives(int lives) {
         this.lives = lives;
     }
 
+    public void update() {
+
+        // TODO: any updates the player has should occur here, 
+        // including movemet and collision detection.
+        // Should be polled every loop iteration
+    }
 }
